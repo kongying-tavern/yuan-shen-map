5e1ab61b199531979c3c39ed044e4746e4e485f5
aaaaaaa8 <790489566@qq.com>
2020-10-25 21:53:22 +0800

    Add

--- Yuanshenditu/js/gitFunctions.js
+++ Yuanshenditu/js/gitFunctions.js
@@ -12,6 +12,9 @@ var user_fileContents = [];
 /** @type {Array.<string>} */
 var user_fileIds = [];
 
+/** @type {Array.<object>} */
+var user_files = [];
+
 /** @type {string} 用户 token 参数*/
 var tokenPara;
 
@@ -31,7 +34,7 @@ function jumpLogin() {
 function getToken() {
   if (getCookie("gitee_Token") != "notfound") {
     tokenPara = getCookie("gitee_Token");
-    getFileList();
+    getGistList();
     return true;
   } else {
     return false;
@@ -116,6 +119,7 @@ function GetQueryString(name) {
  */
 function getFileList() {
   var url = "/database/file/getFileList";
+  //var url = "/giteegist/";
   var access_token = "";
 
   if (tokenPara !== null) {
@@ -142,13 +146,278 @@ function getFileList() {
     alert(msg);
   };
   $.ajax({
-    async: false,
+    async: true,
     type: "POST",
     url: url,
     data: JSON.stringify(data),
     contentType: "application/json",
     success: success,
   });
+}
+
+/**
+ * 日期转化
+ *
+ */
+function formatDate(date) {
+  var y = date.getFullYear()
+  var m = date.getMonth() + 1
+  m = m < 10 ? '0' + m : m
+  var d = date.getDate()
+  d = d < 10 ? '0' + d : d
+  var h = date.getHours()
+  h = h < 10 ? '0' + h : h
+  var minute = date.getMinutes()
+  minute = minute < 10 ? '0' + minute : minute
+  var second = date.getSeconds()
+  second = second < 10 ? '0' + second : second
+  return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second
+}
+
+/**
+ * giteeGist获取文件列表
+ *
+ */
+function getGistList() {
+  var access_token = "";
+  if (tokenPara !== null) {
+    access_token = tokenPara;
+  }
+  var url = "/giteegist/?access_token=" + access_token;
+  var success = function (res) {
+    user_files = [];
+    console.log(res);
+    for (let obj of res) {
+      let currentKey = Object.keys(obj.files)[0];
+      if (currentKey == "Data_KYJG") {
+        var currentData = obj.files[Object.keys(obj.files)[0]].content;
+        //console.log(currentData);
+        var lastUpdateTime = formatDate(new Date(obj.updated_at));
+        //console.log(formatDate(lastUpdateTime));
+        var description = obj.description;
+        //console.log(description);
+        var id = obj.id;
+        //console.log(id);
+        var tempFile = {
+          id: id,
+          description: description,
+          lastUpdateTime: lastUpdateTime,
+          data: currentData
+        }
+        user_files.push(tempFile);
+      }
+    }
+    console.log(user_files);
+    window.frames[0].postMessage({
+      message: "refreshGistList",
+      files: user_files,
+      userName: res[0].owner.name
+    }, '*')
+  };
+  $.ajax({
+    async: true,
+    type: "GET",
+    url: url,
+    contentType: "application/json",
+    success: success,
+  });
+}
+
+/**
+ *修改存档后同步保存本地设置 by giteeGist
+ *
+ *@param fileID {string} 当前需要上传的存档ID
+ *
+ * tips: 上传完成会调用 getGistList() 同步数据
+ */
+function updateLocalData(fileID) {
+  var url = "/giteegist/" + fileID + "?access_token=" + tokenPara;
+  var success = function (res) {
+    console.log(res);
+    var lastUpdateTime = formatDate(new Date(res.updated_at));
+    localStorage.setItem("lastUpdateTime", lastUpdateTime);
+    localStorage.setItem("lastUpdateID", fileID);
+    alert("保存成功！");
+    getGistList();
+  };
+  $.ajax({
+    async: false,
+    type: "GET",
+    url: url,
+    contentType: "application/json",
+    success: success,
+  });
+}
+
+/**
+ *修改存档 by giteeGist
+ *
+ *@param fileID {string} 当前需要上传的存档ID
+ *
+ * tips: 上传完成会调用 getGistList() 同步数据
+ */
+function updateGistFile(fileID) {
+  var markersData = [];
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i); //获取本地存储的Key
+    // @ts-ignore
+    if (localStorage.getItem(key) == 1) markersData.push(key); //所有value
+  }
+  var success = function (res) {
+    updateLocalData(fileID);
+  };
+  let err = function (msg) {
+    console.log(msg);
+  };
+  var markersJsonData = {
+    content: JSON.stringify(markersData)
+  };
+  console.log(JSON.stringify(markersJsonData));
+  var url = "/giteegist/" + fileID;
+  var fileName = "Data_KYJG";
+  var data = '{"access_token": "' + tokenPara + '","files": {"' + fileName + '": ' + JSON.stringify(markersJsonData) + '}}';
+  $.ajax({
+    async: false,
+    type: "PATCH",
+    url: url,
+    data: JSON.stringify(JSON.parse(data)),
+    contentType: "application/json",
+    success: success,
+    error: err,
+  });
+}
+
+/**
+ *新建存档 by giteeGist
+ *
+ *@param sync {string} 当前上传的存档是否需要直接传入本地存档
+ * 
+ * tips: 新建完成会调用 getGistList() 同步数据
+ */
+function addGistFile(sync) {
+  var markersData = [];
+  if (sync == "false") {
+    for (var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i); //获取本地存储的Key
+      // @ts-ignore
+      if (localStorage.getItem(key) == 1) markersData.push(key); //所有value
+    }
+  }
+  var markersJsonData = {
+    content: JSON.stringify(markersData)
+  };
+  var description = window.prompt("请输入存档名");
+  if (description) {
+    var success = function (res) {
+      alert("新建完成");
+      getGistList();
+    };
+    var url = "/giteegist/";
+    var fileName = "Data_KYJG";
+    var data = '{"access_token": "' + tokenPara + '","description":"' + description + '","files": {"' + fileName + '": ' + JSON.stringify(markersJsonData) + '}}';
+    console.log(JSON.stringify(JSON.parse(data)));
+    $.ajax({
+      async: false,
+      type: "POST",
+      url: url,
+      data: JSON.stringify(JSON.parse(data)),
+      contentType: "application/json",
+      success: success,
+    });
+  }
+}
+
+/**
+ *修改存档备注 by giteeGist
+ *
+ *@param fileID {string} 需要修改的存档ID
+ *
+ * tips: 修改完成会调用 getGistList() 同步数据
+ */
+function updateGistDescription(fileID) {
+  var description = window.prompt("请输入存档名");
+  if (description) {
+    var success = function (res) {
+      console.log(res);
+      if (res.id == fileID) {
+        let lastUpdateTime = formatDate(new Date(res.updated_at));
+        localStorage.setItem("lastUpdateTime", lastUpdateTime);
+      }
+      alert("修改备注成功！");
+      getGistList();
+    };
+    let err = function (msg) {
+      console.log(msg);
+    };
+    var url = "/giteegist/" + fileID;
+    var data = '{"access_token": "' + tokenPara + '","description":"' + description + '"}';
+    $.ajax({
+      async: false,
+      type: "PATCH",
+      url: url,
+      data: JSON.stringify(JSON.parse(data)),
+      contentType: "application/json",
+      success: success,
+      error: err,
+    });
+  }
+}
+
+/**
+ *删除存档 by giteeGis
+ *
+ *@param fileID {string} 需要删除的存档ID
+ *
+ * tips: 删除完成会调用 getGistList() 同步数据
+ * 删除会丢失之前的存档，如需要请加不可恢复提示
+ */
+function deleteGistFile(fileID) {
+  var msg = "存档删除后不可恢复，您确定要删除这个存档吗？";
+  if (confirm(msg) == true) {
+    var success = function (res) {
+      console.log(res);
+      alert("删除完成！");
+      getGistList();
+    };
+    let err = function (msg) {
+      console.log(msg);
+    };
+    var url = "/giteegist/" + fileID + "?access_token=" + tokenPara;
+    $.ajax({
+      async: false,
+      type: "DELETE",
+      url: url,
+      contentType: "application/json",
+      success: success,
+      error: err,
+    });
+  }
+}
+
+/**
+ *加载存档 by giteeGis
+ *
+ *@param fileID {string} 需要加载的存档ID
+ *
+ * tips: 删除完成会调用 getGistList() 同步数据
+ * 导入会丢失现在存档，如需要请加备份提示
+ * 
+ */
+function loadGistFile(fileID, fileLastUpdateTime) {
+  for (let file of user_files) {
+    if (file.id == fileID) {
+      localStorage.clear();
+      // @ts-ignore
+      var markerLogArr = eval(file.data);
+      for (var i = 0; i < markerLogArr.length; i++) {
+        localStorage.setItem(markerLogArr[i], "1");
+      }
+      localStorage.setItem("lastUpdateTime", fileLastUpdateTime);
+      localStorage.setItem("lastUpdateID", fileID);
+      alert("导入该存档成功!");
+      getGistList();
+    }
+  }
 }
 
 /**
@@ -255,10 +524,12 @@ function updateFile(fileName, fileID) {
   let err = function (msg) {
     console.log(msg);
   };
-  var markersJsonData ={content: JSON.stringify(markersData)};
+  var markersJsonData = {
+    content: JSON.stringify(markersData)
+  };
   console.log(JSON.stringify(markersJsonData));
   var url = "/giteegist/" + fileID;
-  var data = '{"access_token": "'+tokenPara+'","files": {"'+fileName+'": '+JSON.stringify(markersJsonData)+'}}';
+  var data = '{"access_token": "' + tokenPara + '","files": {"' + fileName + '": ' + JSON.stringify(markersJsonData) + '}}';
   $.ajax({
     async: false,
     type: "PATCH",
