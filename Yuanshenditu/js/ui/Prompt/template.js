7371abea7066a275b1acd9ffdb9726ecd6203074
ZengJia <zengjia@stelulo.com>
2021-04-01 17:42:28 +0800

    !5 修复弹窗移动端BUG * Merge branch 'master' of https://gitee.com/zengjias/yuan-shen-map * 删除文件 Yuanshenditu/js/Prompt/.keep * 新建 Prompt * 修复移动端BUG

---
+++ Yuanshenditu/js/ui/Prompt/template.js
@@ -0,0 +1,34 @@
+class Template {
+  promptView(options) {
+    const { title, content, background, radius, version, lastEditTime, about } = options;
+    const myPrompt = document.createElement("div");
+    myPrompt.className = "prompt prompt-hide";
+    myPrompt.innerHTML = `
+						<div class="prompt-inner" style="border-radius:${radius}px;transform: none;">
+							<span class="prompt-close-btn" role="button" title="サイドバーを閉じます">
+								<i class="prompt-close-icon" >
+									
+								</i>
+							</span>
+							<div class="prompt-title">
+								<h2>${title}</h2>
+							</div>
+							<div class="prompt-content">
+								<p class="prompt-content-version">${version}</p>
+								<p class="prompt-content-about">
+									${about}
+								</p>
+								<div class="prompt-content-main">
+									<P class="prompt-content-main-lastedittime">
+										最終更新時間: <time>${lastEditTime}</time>
+									</p>
+									${content}
+								</div>
+							</div>
+						</div>
+					`;
+    return myPrompt;
+  }
+}
+
+export default Template;
