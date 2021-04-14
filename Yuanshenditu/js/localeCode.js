b3e70f9f4a6f80760511cf57ec5993a069cc7b31
ZengJia <zengjia@stelulo.com>
2021-04-14 13:08:16 +0800

    !11 增加自动跳转页(Loading.html), 优化SEO * Merge branch 'master' of gitee.com:blacklotusccw/yuan-shen-map into master * 增加自动跳转页, 优化多语言SEO * 优化多语言SEO * 优化facebook和twitter的等主流社交软件的SEO和分享, 修复更新日志的弹窗特殊情况时鬼畜的问题

---
+++ Yuanshenditu/js/localeCode.js
@@ -0,0 +1,575 @@
+// @ts-nocheck
+if(window.localeCode){
+  localeCode.data = [
+    {
+      code: "af",
+      type: "南非荷兰语",
+    },
+    {
+      code: "ar-sa",
+      type: "阿拉伯语",
+      country: "沙特阿拉伯",
+    },
+    {
+      code: "ar-eg",
+      type: "阿拉伯语",
+      country: "埃及",
+    },
+    {
+      code: "ar-dz",
+      type: "阿拉伯语",
+      country: "阿尔及利亚",
+    },
+    {
+      code: "ar-tn",
+      type: "阿拉伯语",
+      country: "突尼斯",
+    },
+    {
+      code: "ar-ye",
+      type: "阿拉伯语",
+      country: "也门",
+    },
+    {
+      code: "ar-jo",
+      type: "阿拉伯语",
+      country: "约旦",
+    },
+    {
+      code: "ar-kw",
+      type: "阿拉伯语",
+      country: "科威特",
+    },
+    {
+      code: "ar-bh",
+      type: "阿拉伯语",
+      country: "巴林",
+    },
+    {
+      code: "eu",
+      type: "巴斯克语",
+    },
+    {
+      code: "be",
+      type: "贝劳语",
+    },
+    {
+      code: "zh-tw",
+      type: "中文",
+      country: "中国台湾",
+    },
+    {
+      code: "zh-hk",
+      type: "中文",
+      country: "中国香港特别行政区",
+    },
+    {
+      code: "hr",
+      type: "克罗地亚语",
+    },
+    {
+      code: "da",
+      type: "丹麦语",
+    },
+    {
+      code: "nl-be",
+      type: "荷兰语",
+      country: "比利时",
+    },
+    {
+      code: "en-us",
+      type: "英语",
+      country: "美国",
+    },
+    {
+      code: "en-au",
+      type: "英语",
+      country: "澳大利亚",
+    },
+    {
+      code: "en-nz",
+      type: "英语",
+      country: "新西兰",
+    },
+    {
+      code: "en-za",
+      type: "英语",
+      country: "南非",
+    },
+    {
+      code: "en",
+      type: "英语",
+      country: "加勒比",
+    },
+    {
+      code: "en-tt",
+      type: "英语",
+      country: "特立尼达",
+    },
+    {
+      code: "fo",
+      type: "法罗语",
+    },
+    {
+      code: "fi",
+      type: "芬兰语",
+    },
+    {
+      code: "fr-be",
+      type: "法语",
+      country: "比利时",
+    },
+    {
+      code: "fr-ch",
+      type: "法语",
+      country: "瑞士",
+    },
+    {
+      code: "gd",
+      type: "盖尔语",
+      country: "苏格兰",
+    },
+    {
+      code: "de",
+      type: "德语",
+    },
+    {
+      code: "de-at",
+      type: "德语",
+      country: "奥地利",
+    },
+    {
+      code: "de-li",
+      type: "德语",
+      country: "列支敦士登",
+    },
+    {
+      code: "he",
+      type: "希伯来语",
+    },
+    {
+      code: "hu",
+      type: "匈牙利语",
+    },
+    {
+      code: "in",
+      type: "印度尼西亚语",
+    },
+    {
+      code: "it-ch",
+      type: "意大利语",
+      country: "瑞士",
+    },
+    {
+      code: "ko",
+      type: "朝鲜语",
+    },
+    {
+      code: "lv",
+      type: "拉脱维亚语",
+    },
+    {
+      code: "mk-FYRO",
+      type: "马其顿语",
+    },
+    {
+      code: "mt",
+      type: "马耳他语",
+    },
+    {
+      code: "no",
+      type: "挪威语",
+      country: "尼诺斯克",
+    },
+    {
+      code: "pt-br",
+      type: "葡萄牙语",
+      country: "巴西",
+    },
+    {
+      code: "rm",
+      type: "拉丁语系",
+    },
+    {
+      code: "ro-mo",
+      type: "罗马尼亚语",
+      country: "摩尔达维亚",
+    },
+    {
+      code: "ru-mo",
+      type: "俄语",
+      country: "摩尔达维亚",
+    },
+    {
+      code: "sr",
+      type: "塞尔维亚语",
+      country: "西里尔",
+    },
+    {
+      code: "sk",
+      type: "斯洛伐克语",
+    },
+    {
+      code: "sb",
+      type: "索布语",
+    },
+    {
+      code: "es-mx",
+      type: "西班牙语",
+      country: "墨西哥",
+    },
+    {
+      code: "es-gt",
+      type: "西班牙语",
+      country: "危地马拉",
+    },
+    {
+      code: "es-pa",
+      type: "西班牙语",
+      country: "巴拿马",
+    },
+    {
+      code: "es-ve",
+      type: "西班牙语",
+      country: "委内瑞拉",
+    },
+    {
+      code: "es-pe",
+      type: "西班牙语",
+      country: "秘鲁",
+    },
+    {
+      code: "es-ec",
+      type: "西班牙语",
+      country: "厄瓜多尔",
+    },
+    {
+      code: "es-uy",
+      type: "西班牙语",
+      country: "乌拉圭",
+    },
+    {
+      code: "es-bo",
+      type: "西班牙语",
+      country: "玻利维亚",
+    },
+    {
+      code: "es-hn",
+      type: "西班牙语",
+      country: "洪都拉斯",
+    },
+    {
+      code: "es-pr",
+      type: "西班牙语",
+      country: "波多黎各",
+    },
+    {
+      code: "sv",
+      type: "瑞典语",
+    },
+    {
+      code: "th",
+      type: "泰语",
+    },
+    {
+      code: "tn",
+      type: "瓦纳语",
+    },
+    {
+      code: "uk",
+      type: "乌克兰语",
+    },
+    {
+      code: "ve",
+      type: "文达语",
+    },
+    {
+      code: "xh",
+      type: "科萨语",
+    },
+    {
+      code: "sq",
+      type: "阿尔巴尼亚语",
+    },
+    {
+      code: "ar-iq",
+      type: "阿拉伯语",
+      country: "伊拉克",
+    },
+    {
+      code: "ar-ly",
+      type: "阿拉伯语",
+      country: "利比亚",
+    },
+    {
+      code: "ar-ma",
+      type: "阿拉伯语",
+      country: "摩洛哥",
+    },
+    {
+      code: "ar-om",
+      type: "阿拉伯语",
+      country: "阿曼",
+    },
+    {
+      code: "ar-sy",
+      type: "阿拉伯语",
+      country: "叙利亚",
+    },
+    {
+      code: "ar-lb",
+      type: "阿拉伯语",
+      country: "黎巴嫩",
+    },
+    {
+      code: "ar-ae",
+      type: "阿拉伯语",
+      country: "阿拉伯联合酋长国",
+    },
+    {
+      code: "ar-qa",
+      type: "阿拉伯语",
+      country: "卡塔尔",
+    },
+    {
+      code: "bg",
+      type: "保加利亚语",
+    },
+    {
+      code: "ca",
+      type: "加泰罗尼亚语",
+    },
+    {
+      code: "zh-cn",
+      type: "中文",
+      country: "中国大陆",
+    },
+    {
+      code: "zh-sg",
+      type: "中文",
+      country: "新加坡",
+    },
+    {
+      code: "cs",
+      type: "捷克语",
+    },
+    {
+      code: "nl",
+      type: "荷兰语",
+    },
+    {
+      code: "en",
+      type: "英语",
+    },
+    {
+      code: "en-gb",
+      type: "英语",
+      country: "英国",
+    },
+    {
+      code: "en-ca",
+      type: "英语",
+      country: "加拿大",
+    },
+    {
+      code: "en-ie",
+      type: "英语",
+      country: "爱尔兰",
+    },
+    {
+      code: "en-jm",
+      type: "英语",
+      country: "牙买加",
+    },
+    {
+      code: "en-bz",
+      type: "英语",
+      country: "伯利兹",
+    },
+    {
+      code: "et",
+      type: "爱沙尼亚语",
+    },
+    {
+      code: "fa",
+      type: "波斯语",
+    },
+    {
+      code: "fr",
+      type: "法语",
+    },
+    {
+      code: "fr-ca",
+      type: "法语",
+      country: "加拿大",
+    },
+    {
+      code: "fr-lu",
+      type: "法语",
+      country: "卢森堡",
+    },
+    {
+      code: "gd-ie",
+      type: "盖尔语",
+      country: "爱尔兰",
+    },
+    {
+      code: "de-ch",
+      type: "德语",
+      country: "瑞士",
+    },
+    {
+      code: "de-lu",
+      type: "德语",
+      country: "卢森堡",
+    },
+    {
+      code: "el",
+      type: "希腊语",
+    },
+    {
+      code: "hi",
+      type: "北印度语",
+    },
+    {
+      code: "is",
+      type: "冰岛语",
+    },
+    {
+      code: "it",
+      type: "意大利语",
+    },
+    {
+      code: "ja",
+      type: "日语",
+    },
+    {
+      code: "ko",
+      type: "朝鲜语",
+      country: "韩国",
+    },
+    {
+      code: "lt",
+      type: "立陶宛语",
+    },
+    {
+      code: "ms",
+      type: "马来西亚语",
+    },
+    {
+      code: "no",
+      type: "挪威语",
+      country: "博克马尔",
+    },
+    {
+      code: "pl",
+      type: "波兰语",
+    },
+    {
+      code: "pt",
+      type: "葡萄牙语",
+      country: "葡萄牙",
+    },
+    {
+      code: "ro",
+      type: "罗马尼亚语",
+    },
+    {
+      code: "ru",
+      type: "俄语",
+    },
+    {
+      code: "sz",
+      type: "萨摩斯语",
+      country: "拉普兰",
+    },
+    {
+      code: "sr",
+      type: "塞尔维亚语",
+      country: "拉丁",
+    },
+    {
+      code: "sl",
+      type: "斯洛文尼亚语",
+    },
+    {
+      code: "es",
+      type: "西班牙语",
+      country: "西班牙传统",
+    },
+    {
+      code: "es",
+      type: "西班牙语",
+      country: "西班牙现代",
+    },
+    {
+      code: "es-cr",
+      type: "西班牙语",
+      country: "哥斯达黎加",
+    },
+    {
+      code: "es-do",
+      type: "西班牙语",
+      country: "多米尼加共和国",
+    },
+    {
+      code: "es-co",
+      type: "西班牙语",
+      country: "哥伦比亚",
+    },
+    {
+      code: "es-ar",
+      type: "西班牙语",
+      country: "阿根廷",
+    },
+    {
+      code: "es-cl",
+      type: "西班牙语",
+      country: "智利",
+    },
+    {
+      code: "es-py",
+      type: "西班牙语",
+      country: "巴拉圭",
+    },
+    {
+      code: "es-sv",
+      type: "西班牙语",
+      country: "萨尔瓦多",
+    },
+    {
+      code: "es-ni",
+      type: "西班牙语",
+      country: "尼加拉瓜",
+    },
+    {
+      code: "sx",
+      type: "苏图语",
+    },
+    {
+      code: "sv-fi",
+      type: "瑞典语",
+      country: "芬兰",
+    },
+    {
+      code: "ts",
+      type: "汤加语",
+    },
+    {
+      code: "tr",
+      type: "土耳其语",
+    },
+    {
+      code: "ur",
+      type: "乌尔都语",
+    },
+    {
+      code: "vi",
+      type: "越南语",
+    },
+    {
+      code: "ji",
+      type: "依地语",
+    },
+  ];
+}
