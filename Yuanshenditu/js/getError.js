77128750e2f6ff969bd867d9ff4b85323a3077aa
(^_^) <zengjia@stelulo.com>
2021-05-23 21:58:23 +0800

    完善getError.js

--- Yuanshenditu/js/getError.js
+++ Yuanshenditu/js/getError.js
@@ -1,21 +1,20 @@
 /*
  * @Author       : (*^_^*)
- * @LastEditTime : 2021-05-21 6:49 PM
+ * @LastEditTime : 2021-05-23 9:39 PM
  * @Description  : SaveErrorLog
  */
 
   "use strict";
-  const NAMESPACE = `${window.location.pathname.substring(1).toLocaleUpperCase()}@ERRORLLOG`;
-  
+  const NAMESPACE = `${(window.location.pathname.substring(1) || "index.html").toLocaleUpperCase()}@ERRORLLOG`;
+  const SDK_VERSION = `1.0.1_Alpha`;
+
   injecJsError(); // 捕获Js错误
   injectFetch();  // 捕获Fetch错误
   injectXhr();    // 捕获Ajax错误
   blackScreen();  // 捕获白屏错误
-
-  function core() {
-
-  }
-
+  injectNetWork(); // 捕获网络连接状态
+  injectConsole("error", "warn"); // 捕获console.error()和console.warn()的值
+  
   function injecJsError() {
     window.addEventListener("error", function (event) {
       let lastEvent = getLastEvent();
@@ -26,25 +25,29 @@
           effectiveType,
           rtt,
         } = connection;
-        saveItem({
+        saveItem("stability", {
           type: "resourceError",
           message: event.message,
-          filters: event.target.src || event.target.href,
+          filename: event.target.src || event.target.href,
           tagNmae: event.target.tagName,
-          selector: getSelector(event.target),
+          isTrusted: event.isTrusted,
+          selector: getSelector(event.path || event.target),
+          timeStamp: event.timeStamp,
+          deepPath: event.deepPath,
+          eventPhase: event.eventPhase,
           effectiveType,
           downlink,
           rtt,
         });
       } else {
-        saveItem({
+        saveItem("stability", {
           type: "JsError",
           message: event.message,
-          filters: event.filename,
-          position: `${event.lineno}:${event.colno}`,
+          filename: event.filename,
+          position: `${event.lineno || 0}:${event.colno || 0}`,
           state: getLines(event.error.stack),
           name: event.error.name,
-          selector: lastEvent ? getSelector(lastEvent.path) : ""
+          selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : null
         });
       }
     }, true);
@@ -56,11 +59,13 @@
       let line = 0;
       let column = 0;
       let stack = "";
+      if (typeof event.reason === "string") {
+        message = event.reason;
+      } else if (typeof event.reason === "object") {
+        message = event.reason.message;
+      }
       let reason = event.reason;
-      if (typeof reason === "string") {
-        message = reason;
-      } else if (typeof reason === "object") {
-        message = reason.message;
+      if (typeof reason === "object") {
         if (reason.stack) {
           let matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/);
           filename = matchResult[1];
@@ -69,17 +74,30 @@
         }
         stack = getLines(reason.stack);
       }
-      saveItem({
-        type: "PromiseError",
+      saveItem("stability", {
+        type: "promiseError",
         message,
         filename,
         stack,
-        position: `${ line}:${column}`,
-        stack: getLines(event.error.stack),
-        selector: lastEvent ? getSelector(lastEvent.path) : ""
+        position: `${line}:${column}`,
+        selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : null
       })
     }, true);
   }
+
+  function injectConsole(...value){
+    value.forEach((val) =>{
+      const consoleError = window.console[val];
+      window.console[val] = function () {
+        saveItem("stability", {
+          type: `Console${firstUpperCase(val)}`,
+          message: arguments
+        })
+        consoleError && consoleError.apply(window, arguments);
+      };
+    });
+  }
+
 
   function injectXhr() {
     let XMLHttpRequset = window.XMLHttpRequest;
@@ -98,7 +116,7 @@
             effectiveType,
             rtt,
           } = connection;
-          saveItem({
+          saveItem("stability", {
             type: "xhrError",
             eventType: type,
             pathname: this.logData.url,
@@ -132,11 +150,11 @@
       return _oldFetch.apply(this, arguments)
         .then(res => {
           if (!res.ok) { 
-            saveItem({
+            saveItem("stability", {
               type: "fetchStateError",
               url: res.url,
-              redirected: res.redirect,
               requestType: res.type,
+              redirected: res.redirected,
               status: res.status,
               text: res.statusText,
               downlink,
@@ -147,13 +165,13 @@
           return res;
         })
         .catch(error => {
-          saveItem({
+          console.dir(error);
+          saveItem("stability", {
             type: "fetchError",
-            url: res.url,
-            requestType: res.type,
-            redirected: res.redirect,
-            status: res.status,
-            text: res.statusText,
+            message: error.message,
+            position: `${error.lineno || 0}:${error.colno || 0}`,
+            state: getLines(error.stack),
+            name: error.name,
             downlink,
             effectiveType,
             rtt,
@@ -163,6 +181,101 @@
     }
   }
 
+  function injectNetWork() {
+    if(typeof window.navigator.onLine === "undefined") return;
+    window.addEventListener("offline", function (e) {
+      saveItem("stability", {
+        type: "netWork",
+        isOnline: false,
+        timeStamp: e.timeStamp,
+      });
+    }, false);
+    
+    window.addEventListener("online", function (e) {
+      const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+      const {
+        downlink,
+        downlinkMax,
+        effectiveType,
+        onchange,
+        ontypechange,
+        rtt,
+        saveData,
+        type,
+      } = connection;
+      saveItem("stability", {
+        type: "netWork",
+        isOnline: true,
+        timeStamp: e.timeStamp,
+        netWorktype: type,
+        downlink,
+        downlinkMax,
+        effectiveType,
+        onchange,
+        ontypechange,
+        rtt,
+        saveData,
+      });
+    },false)
+  }
+  
+  function Performance(){
+    if (!window.performance) return console.warn("Browsers do not support performance!");
+    this.performance = window.performance;
+    this.performanceGetEntries = window.performance.getEntries();
+    let result = {
+      type: "performance",
+      
+    };
+  }
+  
+  Performance.prototype.memory = function () {
+    if (!performance.memory) return console.warn("Browsers do not support memory!");
+      const {
+        jsHeapSizeLimit,
+        totalJSHeapSize,
+        usedJSHeapSize,
+      } = performance.memory;
+      saveItem("experience", {
+        type: "memory",
+        jsHeapSizeLimit,
+        totalJSHeapSize,
+        usedJSHeapSize,
+      })
+  }
+  Performance.prototype.cssLoadTime = function () {
+    const p = this.performanceGetEntries;
+    let cssR = p.filter(ele => ele.initiatorType === "css");
+    return Math.max(...cssR.map((ele) => ele.responseEnd)) - Math.min(...cssR.map((ele) => ele.startTime));
+  }
+  
+  Performance.prototype.jsLoadTime = function () {
+    const p = this.performanceGetEntries;
+    let cssR = p.filter(ele => ele.initiatorType === "script");
+    return Math.max(...cssR.map((ele) => ele.responseEnd)) - Math.min(...cssR.map((ele) => ele.startTime));
+  }
+
+  Performance.prototype.longTask = function() {
+    new PerformanceObserver((list) => {
+      list.getEntries().forEach(entry => {
+        if (entry.duration > 100) {
+          let lastEvent = getLastEvent();
+          requestIdleCallback(() => {
+            saveItem("experience", {
+              type: "longTask",
+              eventType: lastEvent.type,
+              startTime: formatTime(entry.startTime), // 开始时间
+              duration: formatTime(entry.duration), // 持续时间
+              selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : ""
+            });
+          });
+        }
+      });
+    }).observe({
+      entryTypes: ["longtask"]
+    });
+  }
+  
   function blackScreen(filtrationElement, emptyPointsCount, blankPointThreshold) {
     const wrapperElements = filtrationElement || ["html", "body"]; //过滤元素数组
     const myBlankPointThreshold = blankPointThreshold || 16;
@@ -188,7 +301,7 @@
         let centerElements = document.elementFromPoint(
           window.innerWidth / 2, window.innerHeight / 2
         );
-        saveItem({
+        saveItem("stability", {
           type: "blackScreen",
           emptyPoints,
           selector: getSelector(centerElements[0]),
@@ -197,23 +310,45 @@
       }
     })
   }
+  
   // Utils Function
 
-  function getSelector(element) {
-    let selector;
-    if (typeof element === "undefined") return "unknown";
-    if (typeof element.id !== "undefined" && element.id !== "") {
-      selector = `#${element.id}`;
-    } else if (element.className && typeof element.className === 'string') {
-      selector = '.' + element.className.split(' ').filter(function (item) {
-        return !!item
-      }).join('.');
-    } else {
-      selector = element.nodeName.toLowerCase();
+  function getSelector(pathsOrTarget) {
+    const getMySelector = function (path) {
+      return path.reverse().filter(function (element) {
+        return element !== window && element !== document;
+      }).map(function (element) {
+        var selector;
+        if (element.id) {
+          selector = `#${element.id}`;
+        } else if (element.className && typeof element.className === "string") {
+          selector = "." + element.className.split(" ").filter(function (item) {
+            return !!item
+          }).join(".");
+        } else {
+          selector = element.nodeName;
+        }
+        return selector;
+      }).join(" ");
     }
-    return selector;
+
+    if (Array.isArray(pathsOrTarget)) {
+      return getMySelector(pathsOrTarget);
+    } else {
+      var paths = [];
+      var element = pathsOrTarget;
+      while (element) {
+        paths.push(element);
+        element = element.parentNode;
+      }
+      return getMySelector(paths);
+    }
   }
 
+  function firstUpperCase(str) {
+    return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+  }
+  
   function getLastEvent() {
     let lastEvent = "";
     const str = ["click", "touchstart", "mousedown", "keydown", "mouseover"];
@@ -242,6 +377,7 @@
   }
 
   function getLines(stack) {
+    if (!stack) return "";
     return stack.split("\n").slice(1).map(item => item.replace(/^\s+at\s+/g, "")).join("^");
   }
 
@@ -262,30 +398,37 @@
     }
   }
 
-  function saveItem(value) {
+  function saveItem(key, value) {
     let storage = window.localStorage.getItem(NAMESPACE);
-    if (!storage) {
-      storage = [];
+    if(!storage){
+      storage = {
+        [key]: []
+      }
     } else {
       storage = JSON.parse(storage);
+      if (!storage.storage[key]) storage.storage[key] = [];
       const date = new Date(storage.date);
       storage = storage.storage;
+      const timeDifference = Math.floor(new Date(new Date().getTime() - date.getTime()));
       // 每次存入时检查, 距离上一次存超过三十分钟就删除
-      if (new Date().getTime() - new Date(date).getTime() >= 60 * 30000) {
+      if (timeDifference >= 1000 * 60 * 30) {
         window.localStorage.removeItem(NAMESPACE);
-        console.log(`delete errorlog succeed\nLastSavedDate: ${new Date(date).toLocaleString()}`);
+        console.log(`delete errorlog succeed\nlastSavedDate: ${date.toLocaleString()}`);
       }
     }
-    storage.push({
+    storage[key].push({
       id: goodid(8),
       data: {
-        ...value,
+        ...filtration(value),
       },
-      date: new Date().toLocaleString('zh-CN')
+      date: new Date().toLocaleString("zh-CN", {
+        hour12: false,
+        timeZone: "UTC"
+      })
     });
     window.localStorage.setItem(NAMESPACE, JSON.stringify({
       storage,
-      date: new Date()
+      date: new Date(),
     }));
   }
 
@@ -326,7 +469,7 @@
       viewPixelRatio: `${parseInt(innerW * ratio)}*${parseInt(innerH * ratio)}`, //逻辑分辨率
       colorDepth: typeof screen.colorDepth !== "undefined" ? `${screen.colorDepth}` : null, //目标设备或缓冲器上的调色板的比特深度
       pixelDepth: typeof screen.pixelDepth !== "undefined" ? `${screen.pixelDepth}` : null, //显示屏幕的颜色分辨率（比特每像素）
-      state: `${getOrientationState()}`, //屏幕横竖状态 
+      state: `${getOrientationState() || null}`, //屏幕横竖状态 
     })
   }
 
@@ -352,18 +495,18 @@
 	  return object;
 	}
   (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global.goodid = factory())
+    typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global.goodid = factory())
   }(this, (function () {
-    'use strict';
-    var defaultAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    "use strict";
+    var defaultAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     var defaultStartTime = new Date(2018, 9, 1).getTime();
     var defaultLength = 16;
-    var defaultPrefix = '';
+    var defaultPrefix = "";
 
     function genRandom(length, alphabet) {
       alphabet = alphabet || defaultAlphabet;
       length = length || 1;
-      var id = '';
+      var id = "";
       while (length-- > 0) {
         id += alphabet[Math.random() * alphabet.length | 0]
       }
@@ -371,25 +514,25 @@
     }
 
     function genTimestamp() {
-      var timeArr = Math.round((Date.now() - defaultStartTime) / 1000).toString(36).split('');
+      var timeArr = Math.round((Date.now() - defaultStartTime) / 1000).toString(36).split("");
       var count = 3;
       var index = 0;
       while (count-- > 0) {
         index = Math.random() * timeArr.length | 0;
         timeArr[index] = timeArr[index].toUpperCase()
       }
-      return timeArr.join('')
+      return timeArr.join("")
     }
 
     function goodid(length, prefix, alphabet) {
       length = length || defaultLength;
       prefix = prefix || defaultPrefix;
-      if (length <= prefix.length) throw new Error('The length parameter cannot be less than the length of the prefix.');
+      if (length <= prefix.length) throw new Error("The length parameter cannot be less than the length of the prefix.");
       var time = genTimestamp();
       if (length < 16 || alphabet) {
-        return [prefix, genRandom(length - prefix.length, alphabet)].join('')
+        return [prefix, genRandom(length - prefix.length, alphabet)].join("")
       } else {
-        return [prefix, time, genRandom(length - prefix.length - time.length)].join('')
+        return [prefix, time, genRandom(length - prefix.length - time.length)].join("")
       }
     }
     goodid.defaults = {
