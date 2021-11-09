b6ff7dfdf3051f04c2b1e15b32538d68e0a50254
PeaceShi <peaceshi@outlook.com>
2021-11-09 18:26:50 +0800

    Update genGeoJson.mjs
    fix generate geojson error.

--- tools/genGeoJson.mjs
+++ tools/genGeoJson.mjs
@@ -1,90 +1,97 @@
-// @ts-ignore
-import JS_array from "./data.mjs";
+//// @ts-ignore
+import JS_array from './data.mjs'
 
-import process from "process";
-const url = process.argv[2];
+import process from 'process'
+const url = process.argv[2]
 
-import fs from "fs-extra";
+import fs from 'fs-extra'
 const writeTempStream = async (fileName, data) => {
   try {
-    const tmpDir = await makeTempDir();
-    const filePath = `${tmpDir}${path.sep}${fileName}`;
-    consoleInfo("write temp file.");
-    const wStream = fs.createWriteStream(filePath);
-    wStream.write(data, "utf8");
-    consoleInfo("write temp file succeed.");
-    return filePath;
+    const tmpDir = await makeTempDir()
+    const filePath = `${tmpDir}${path.sep}${fileName}`
+    consoleInfo('write temp file.')
+    const wStream = fs.createWriteStream(filePath)
+    wStream.write(data, 'utf8')
+    consoleInfo('write temp file succeed.')
+    return filePath
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
-import os from "os";
-import path from "path";
+import os from 'os'
+import path from 'path'
 const makeTempDir = async () => {
   try {
-    consoleInfo("make temp dir.");
-    return await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
+    consoleInfo('make temp dir.')
+    return await fs.mkdtemp(`${os.tmpdir()}${path.sep}`)
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
 const consoleInfo = (string) => {
-  console.log(`${string}================================================`);
-};
+  console.log(`${string}================================================`)
+}
 
 const copyFiles = async (src, dest) => {
   try {
-    consoleInfo("start copy file.");
-    await fs.copy(src, dest);
-    consoleInfo("copy succeed.");
+    consoleInfo('start copy file.')
+    await fs.copy(src, dest)
+    consoleInfo('copy succeed.')
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
 const genGeoJson = async (res) => {
-  consoleInfo("start gen geojson file.");
+  consoleInfo('start gen geojson file.')
   for await (const item of res.data.list) {
-    var markerX = item.xy.split(",")[0];
-    var markerY = item.xy.split(",")[1];
+    var markerX = item.xy.split(',')[0]
+    var markerY = item.xy.split(',')[1]
     var markerInfo = {
       geometry: {
-        type: "Point",
+        type: 'Point',
         coordinates: [markerX, markerY],
       },
-      type: "Feature",
+      type: 'Feature',
       properties: {
         popTitle: item.popTitle,
         popupContent: item.popupContent,
       },
       id: item.id,
-    };
-    JS_array[item.layerId].features.push(markerInfo);
+    }
+    JS_array[item.layerId].features.push(markerInfo)
   }
-  consoleInfo("gen geojson file succeed.");
-};
+  consoleInfo('gen geojson file succeed.')
+}
 
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 const fetchData = async () => {
-  consoleInfo("download data.");
+  consoleInfo('fetch data.')
   const rawResponse = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
     },
-  });
-  consoleInfo("download end.");
-  return await rawResponse.json();
-};
+  })
+  consoleInfo('fetch end.')
+  if (!rawResponse.ok) {
+    throw new Error(`HTTP error! status: ${rawResponse.status}`)
+  } else {
+    return await rawResponse.json()
+  }
+}
 
-(async () => {
-  const content = await fetchData();
-  const onSuccess = await genGeoJson(content);
-  await Promise.all([content, onSuccess]).then(async () => {
-    const tmpFilePath = await writeTempStream("marker.js",`var originJsonArr =${JSON.stringify(JS_array)};for (var i = 0; i < originJsonArr.length; i++) { JS_array[i].features = originJsonArr[i].features; }\n// ${Date()}\n`);
-    await copyFiles(tmpFilePath, "./Item_Features.js");
-  });
-})();
+;(async () => {
+  const content = await fetchData().then(async (res) => {
+    await genGeoJson(res)
+    return JSON.stringify(JS_array)
+  })
+  const tmpFilePath = await writeTempStream(
+    'marker.js',
+    `var originJsonArr =${content};\nfor (var i = 0; i < originJsonArr.length; i++) { JS_array[i].features = originJsonArr[i].features; }\n// ${Date()}\n`
+  )
+  await copyFiles(tmpFilePath, './Item_Features.js')
+})()
